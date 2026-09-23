e4a6cb52d64992c2ad8884d71435e04a310f2199
Charles Clarke <charles4clarke@gmail.com>
2026-09-23 16:35:38 -0600

    add (ci and initial tests)

---
+++ src/tests/authRouter.test.js
@@ -0,0 +1,52 @@
+const request = require("supertest");
+const app = require("../service");
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+// async function createAdminUser() {
+//   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + '@admin.com';
+
+//   user = await DB.addUser(user);
+//   return { ...user, password: 'toomanysecrets' };
+// }
+
+const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = randomName() + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test("login", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: "diner" }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+test("get menu as a registered user", async () => {
+  const menuRes = await request(app)
+    .get("/api/order/menu")
+    .set("Authorization", `Bearer ${testUserAuthToken}`);
+
+  expect(menuRes.status).toBe(200);
+  expect(menuRes.body).toEqual(
+    expect.arrayContaining([expect.objectContaining({ title: "Crusty" })]),
+  );
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
+  );
+}
